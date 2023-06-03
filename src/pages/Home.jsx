efc87f14dba651073d860a11decd5ed6c5b09a18
root <root@MSI>
2023-06-03 10:45:40 -0600

    DHA-B73808 CRUD terminado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,68 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import List from '../commons/List';
-import { getFetch, postFetch, putFetch, deleteFetch } from '../commons/ApiMethods';
-import WithLoadingState from '../commons/WithLoadingState';
+import React, { useState } from 'react';
+import Index from '../components/Pets/index';
+import New from '../components/Pets/new';
+import Edit from '../components/Pets/edit';
+import Delete from '../components/Pets/delete';
 
 function Home() {
-  const LoadingList = WithLoadingState(List)
-  const [contents, setContents] = useState([]);
-  const [loading, setLoading] = useState(false);
   const [refresh, setRefresh] = useState(true);
-
-  const [name, setName] = useState('')
-  const [breed, setBreed] = useState('')
-  const [message, setMessage] = useState('')
-
-  useEffect(() => {
-    if (!refresh) return
-    setLoading(true);
-    getFetch('pets').then((data) => {
-      setContents(data)
-      setLoading(false);
-    });
-    setRefresh(false);
-  }, [setContents, setLoading, refresh]);
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      postFetch('pets', {name: name, breed: breed, pedigree: true, owner_id: 1})
-        .then((response) =>{
-          setName('');
-          setBreed('');
-          setMessage('Pet added successfully');
-          setRefresh(true);
-        })
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
   return (
     <div>
-      <h2 style={{ margin: "4px" }}>Primera Conexion con API Local</h2>
-      <LoadingList isLoading={loading} contents={contents} />
+      <Index refresh={refresh} setRefresh={setRefresh}/>
 
-      <form onSubmit={handleSubmit}>
-        <div>
-          <input
-            type='text'
-            value={name}
-            placeholder='Nombre de la mascota'
-            onChange={(e) => {setName(e.target.value)}}/>
-        </div>
-        <div>
-          <input
-            type='text'
-            value={breed}
-            placeholder='Raza de la mascota'
-            onChange={(e) => {setBreed(e.target.value)}}/>
-        </div>
-        <div>{message ? <p>{message}</p> : <br />}</div>
-        <button type='submit'>Crear</button>
-      </form>
+      <New setRefresh={setRefresh}/>
+
+      <Edit setRefresh={setRefresh}/>
+
+      <Delete setRefresh={setRefresh}/>
     </div>
   );
-}
+};
 
 export default Home;

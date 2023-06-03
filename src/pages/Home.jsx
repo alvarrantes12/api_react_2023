86bec3b5497d78f353ae4df9c1de309d8104474d
JoseIsaacLazo <jose.lazo@ucrso.info>
2023-06-03 09:52:50 -0600

    JILL-C04168 Realization of creating, editing and deleting with components in react

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from "react";
+import Index from "../components/Pets/index";
+import New from "../components/Pets/new";
+import Edit from '../components/Pets/edit';
+import Delete from '../components/Pets/delete';
 
 function Home() {
+  const [refresh, setRefresh] = useState(true);
+
   return (
-    <div className='home'>
-      <h1>Home</h1>
+    <div className="">
+      <Index refresh={refresh} setRefresh={setRefresh} />
+      <New setRefresh={setRefresh} />
+      <Edit setRefresh={setRefresh} />
+      <Delete setRefresh={setRefresh} />
     </div>
   );
 }
-
 export default Home;

66963bf62147c67adcf879e06a34d22557b046b7
Victoria <victoria.campos@ucrso.info>
2023-06-03 09:47:27 -0600

    VCF-C08854 Index and New done

---
+++ src/components/Pets/index.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import { getFetch } from '../../commons/ApiMethods';
+import WithLoadingState from '../../commons/WithLoadingState';
+import List from '../../commons/List';
+
+function Index(refresh, setRefresh) {
+    const LoadingList = WithLoadingState(List);
+    const [contents, setContents] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        if (!refresh) return;
+        setLoading(true);
+        getFetch('pets').then((data) => {
+            setContents(data);
+            setLoading(false);
+        });
+        setRefresh(false);
+    }, [setContents, setLoading, refresh]);
+
+    return (
+        <>
+            <h2 style={{ margin: "4px" }}>Primera conexion con API local</h2>
+            <LoadingList isLoading={loading} contents={contents} />
+
+        </>
+
+    )
+
+}
+export default Index;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,89 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { getFetch, postFetch, putFetch, deleteFetch } from '../commons/ApiMethods';
+import Index from '../components/Pets/index'
+import New from '../components/Pets/new';
 function Home() {
+
+  const [refresh, setRefresh] = useState(true);
+
+  const [nameEdit, setNameEdit] = useState('')
+  const [idEdit, setIdEdit] = useState('')
+  const [messageEdit, setMessageEdit] = useState('')
+
+  const [idDelete, setIdDelete] = useState('')
+  const [messageDelete, setMessageDelete] = useState('')
+
+
+  const handleDelete = async (event) => {
+    event.preventDefault();
+    try {
+      deleteFetch(`pets/${idDelete}`)
+        .then(() => {
+          setIdDelete('');
+          setMessageDelete('Eliminado correctamente');
+          setRefresh(true);
+        })
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
+
+  const handleEdit = async (event) => {
+    event.preventDefault();
+    try {
+      putFetch(`pets/${idEdit}`, { name: nameEdit })
+        .then(() => {
+          setNameEdit('')
+          setIdEdit('')
+          setMessageEdit('Editado correctamente')
+          setRefresh(true)
+        })
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <div className='home'>
-      <h1>Home</h1>
+    <div className=''>
+      <Index refresh={refresh} setRefresh={setRefresh} />
+      <New setRefresh={setRefresh} />
+
+      <form onSubmit={handleEdit}>
+        <div>
+          <input
+            type='text'
+            value={idEdit}
+            placeholder='Numero Id'
+            onChange={(e) => { setIdEdit(e.target.value); }} />
+        </div>
+
+        <div>
+          <input
+            type='text'
+            value={nameEdit}
+            placeholder='Nuevo nombre mascota'
+            onChange={(e) => { setNameEdit(e.target.value); }} />
+        </div>
+        <div>{message ? <p>{message}</p> : <br />}</div>
+        <button type='submit'>Editar</button>
+      </form>
+
+      <form onSubmit={handleDelete}>
+        <div>
+          <input
+            type='text'
+            value={idDelete}
+            placeholder='Numero id'
+            onChange={(e) => { setIdDelete(e.target.value); }} />
+        </div>
+
+        <div>{messageDelete ? <p>{messageDelete}</p> : <br />}</div>
+        <button type='submit'>Borrar</button>
+      </form>
+
     </div>
   );
 }

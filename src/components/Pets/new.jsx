369ba60f9da7c2b2003b53bbb5d962aefa6f38b3
FabricioVega <fabricio.vegaalfaro@ucr.ac.cr>
2023-06-03 17:59:16 +0200

    Done

---
+++ src/components/Pets/new.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { postFetch } from '../../commons/ApiMethods';
+
+function New({ setRefresh }) {
+  const [name, setName] = useState('');
+  const [breed, setBreed] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      postFetch('pets', { name: name, breed:breed, pedigree: true, owner_id:1})
+        .then((response) => {
+          setName('');
+          setBreed('');
+          setMessage('Creado correctamente');
+          setRefresh(true);
+        })
+
+    }catch (error){
+      console.log(error)
+    }
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <div>
+            <input 
+                type = 'text'
+                value={name}
+                placeholder='Nombre de la mascota'
+                onChange={(e) => {setName(e.target.value);}} />
+        </div>
+
+        <div>
+            <input refresh
+                type = 'text'
+                value={breed}
+                placeholder='Raza de la mascota'
+                onChange={(e) => {setBreed(e.target.value);}} />
+        </div>
+        <div>{message ? <p>{message}</p> : <br />}</div>
+            <button type='submit'>Crear</button>
+      </form>
+    </>
+  )
+}
+
+export default New;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { deleteFetch } from '../commons/ApiMethods';
+import Index from '../components/Pets/index';
+import New from '../components/Pets/new';
+import Edit from '../components/Pets/edit';
+import Delete from '../components/Pets/delete';
 
 function Home() {
+  const [refresh, setRefresh] = useState(true);
+  
   return (
-    <div className='home'>
-      <h1>Home</h1>
+    <div className=''>
+      <Index refresh={refresh} setRefresh={setRefresh}/>
+      <New refresh={refresh} setRefresh={setRefresh} />
+      <Edit refresh={refresh} setRefresh={setRefresh} />
+      <Delete refresh={refresh} setRefresh={setRefresh} />
     </div>
   );
 }

c910d99e392002f480935e236968fbaa2a81a0be
FerAlvarado99 <maria.alvarado@ucrso.info>
2023-06-03 09:49:02 -0600

    MAA-B70305 Actividad_5

---
+++ src/commons/List.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const List = ({ contents }) => {
+
+  if (!contents || contents.length === 0) return <h1>No hay contenido</h1>
+  return (
+    <div>
+      <h4>Estos son los contenidos disponibles</h4>
+      <table>
+        <tr>
+          <th style={{padding: "0 4em 0 4em"}}>Id</th>
+          <th style={{padding: "0 4em 0 4em"}}>Nombre</th>
+          <th style={{padding: "0 4em 0 4em"}}>Raza</th>
+          <th style={{padding: "0 4em 0 4em"}}>Pedigree</th>
+          <th style={{padding: "0 4em 0 4em"}}>Dueño</th>
+        </tr>
+
+        {contents.map(({ id, name, breed, pedigree, owner: { first_name } }) => (
+          <tr>
+            <td>{id}</td>
+            <td>{name}</td>
+            <td>{breed}</td>
+            <td>{pedigree ? 'Sí' : 'No'}</td>
+            <td>{first_name}</td>
+          </tr>
+        ))
+        }
+
+
+      </table>
+
+
+    </div >
+  )
+}
+export default List

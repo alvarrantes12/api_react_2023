52465be849f628b5bfefe2b24e4f899f0d65ec9d
tyrsergio <sergio.hernandezjimenez@ucr.ac.cr>
2023-06-03 10:18:22 -0600

    SHJ-C13656 yarn Done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,106 @@
-import React from 'react';
+import React, { useState} from 'react';
+import { getFetch, postFetch, putFetch, deleteFetch } from '../commons/ApiMethods';
+import WithLoadingState from '../commons/WithLoadingState';
+import List from '../commons/List';
+import Index from '../components/Pets/index';
+import New from '../components/Pets/new';
 
-function Home() {
+
+function Home(){
+
+  const [refresh, setRefresh] = useState(true)
+
+  const [idEdit, setIdEdit] = useState('')
+  const[nameEdit, setNameEdit] = useState('')
+  const [messageEdit, setMessageEdit] = useState('')
+
+  const [idDelete, setIdDelete] = useState('')
+  const [messageDelete, setMessageDelete] = useState('')
+
+  const handleDelete = async(event) =>{
+      event.preventDefault();
+      try{
+        deleteFetch(`pets/${idDelete}`)
+        .then(() => {
+          setIdDelete ('');
+          setMessageDelete ('Eliminado correctamente');
+          setRefresh (true);
+        })
+      }catch (error) {
+        console.log(error)
+      }
+
+  }
+
+  
+
+  const handleEdit = async (event) => {
+    event.preventDefault();
+
+    try{
+      putFetch(`pets/${idEdit}`, {name: nameEdit} )
+      .then(() =>{
+        setNameEdit('');
+        setIdEdit('');
+        setMessageEdit('editado correctamente');
+        setRefresh(true);
+
+      })
+    }catch(error){
+      console.log(error)
+    }
+  } 
+
+  
+
+  
   return (
-    <div className='home'>
-      <h1>Home</h1>
+    <div className=''>
+      <Index refresh={refresh} setRefresh={setRefresh}/>
+      <New setRefresh={setRefresh}/>
+
+
+      <form onSubmit={handleEdit}>
+        <div>
+          <input
+          type='text'
+          value={idEdit}
+          placeholder='Numero id'
+          onChange={(e) => {setIdEdit(e.target.value);}}/>
+        </div>
+
+        <div>
+          <input
+          type='text'
+          value={nameEdit}
+          placeholder='Nuevo nombre'
+          onChange={(e) => {setNameEdit(e.target.value);}}/>
+        </div>
+        <div>{messageEdit ? <p>{messageEdit}</p> : <br  />}</div>
+        <button type='submit'> Editar </button>
+
+      </form>
+
+      <form onSubmit={handleDelete}>
+        <div>
+          <input
+          type='text'
+          value={idDelete}
+          placeholder='Numero id'
+          onChange={(e) => {setIdDelete(e.target.value);}}/>
+        </div>
+
+        <div>{messageDelete ? <p>{messageDelete}</p> : <br  />}</div>
+        <button type='submit'> Eliminar </button>
+
+      </form>
+
+      
+      
+
+
+
+
     </div>
   );
 }

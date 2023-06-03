a85eca1284a5c12ad2902f1259051617f10521a2
Baigv25 <baigv25@gmail.com>
2023-06-03 09:51:08 -0600

    BGV-C03471 edit, delete, index y create

---
+++ src/commons/List.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const List = ({ contents }) => {
+
+    if (!contents || contents.length === 0) return <p>No hay datos</p>
+
+    return (
+        <div>
+            <h4>Datos Disponibles</h4>
+            <table>
+                <tr>
+                    <th>Id</th>
+                    <th>Nombre</th>
+                    <th>Raza</th>
+                    <th>Pedigree</th>
+                    <th>Dueño</th>
+                </tr>
+
+                {contents.map(({id, name, breed, pedigree, owner: {first_name}}) => (
+                    <tr>
+                        <td>{id}</td>
+                        <td>{name}</td>
+                        <td>{breed}</td>
+                        <td>{pedigree ? "Sí" : "No"}</td>
+                        <td>{first_name}</td>
+                    </tr>
+                ))}
+            </table>
+        </div>
+    )
+
+}
+
+export default List;
